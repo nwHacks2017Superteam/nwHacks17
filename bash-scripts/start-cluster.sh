57e70a56d36ac975132733532f45866eed052fb9
Gareth Ellis <gareth.ellis0@gmail.com>
2017-03-18 19:15:40 -0700

    added script to start clusters

--- bash-scripts/start-cluster.sh
+++ bash-scripts/start-cluster.sh
@@ -2,7 +2,7 @@
 
 # Starts up a cockroach cluster
 
-NUM_SERVERS='' # The number of instances to create in the cluster
+NUM_SERVERS=-1 # The number of instances to create in the cluster
 
 # Flags
 # "-n" the number of instances to create in the cluster
@@ -10,32 +10,42 @@ NUM_SERVERS='' # The number of instances to create in the cluster
 # Get Flags 
 while getopts 'n:' flag; do
     case "${flag}" in
-        n) NUM_SERVERS="${flag}" ;;
+        n) NUM_SERVERS="${OPTARG}" ;;
         *) error "Unexpected option ${flag}" ;;
     esac
 done
 
-if [ "$NUM_SERVERS" == "" ]; then
+if [ $NUM_SERVERS == -1 ]; then
     echo "ERROR: Must provide number of instances via the \"-n\" option"
     exit 1
 fi
 
-# Start an instance, if given an address it will join to that address
-function startInstance()
+# # Start an instance, if given an port it will join to that port
+# function startInstance()
+# {
+#     if [ -z "$1" ]; then 
+#         cockroach start --background --port=0 --http-port=0 --store=node$RANDOM &> /dev/null
+#     else
+#         cockroach start --background --port=0 --http-port=0 --store=node$RANDOM --join=localhost:$1 &> /dev/null 
+#     fi
+# }
+
+function getFreePort ()
 {
-    if [ -z "$1" ]; then 
-        cockroach start --background --port=0 --http-port=0 --store=node$RANDOM
-    else
-        cockroach start --background --port=0 --http-port=0 --store=node$RANDOM --join=$masterInstanceAddress 
-    fi
+    ss -tln | awk 'NR > 1{gsub(/.*:/,"",$4); print $4}' | sort -un | awk -v n=12000 '$0 < n {next}; $0 == n {n++; next}; {exit}; END {print n}'
 }
 
-# Start a main db instance and save the ip it's running on
-test=$(startInstance)
-#echo $test
-#[[ $test =~ 'root@([[:alnum:]]*)' ]]
-##MAIN_INSTANCE_IP=$(./start-instance | sed)
-#
-#echo $MAIN_INSTANCE_IP
+# Start the main db instance and save the port it's running on
+mainPort=$(getFreePort)
+cockroach start --background --port=$mainPort --http-port=0 --store=node$RANDOM > /dev/null
+echo $(ps -fC cockroach | tail -1 | awk '{print $2}')
 
+# Start up however many db instances we asked for
+# and echo the pid of each one
+for i in `eval echo {0..$NUM_SERVERS}`
+do
+    cockroach start --background --port=0 --http-port=0 --store=node$RANDOM --join=localhost:$mainPort > /dev/null
+    # Echo the pid of the just created process
+    echo $(ps -fC cockroach | tail -1 | awk '{print $2}')
+done
 
