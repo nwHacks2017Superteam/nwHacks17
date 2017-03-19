0fd552e53af6125d7ee6f51dfe17e5bca3dfa915
thealexhoar <thealexhoar@gmail.com>
2017-03-18 20:30:16 -0700

    Added attacks.

--- roachapp/public/scripts/game.js
+++ roachapp/public/scripts/game.js
@@ -4,23 +4,38 @@ var player = new createjs.Container();
 var player_hurt_radius = 25;
 
 var roaches = [];
+var roach_layer = new createjs.Container();
+
 var roach_trails = [];
+var trail_layer = new createjs.Container();
 
 var trail_accumulator = 0;
 var trail_interval = 0.2;
 var trail_duration = 3;
 
 var attacks = [];
-var attack_radius = 80;
+var attack_layer = new createjs.Container();
+var hit_radius = 80;
+var attack_duration = 2;
+
+
 
 
 let width, height;
 
+function set_size(w, h) {
+    width = w;
+    height = h;
+}
+
+function set_size_from_canvas(){
+    set_size(stage.canvas.width, stage.canvas.height);
+}
 
 function init() {
     stage = new createjs.Stage("gameCanvas");
-    width = stage.canvas.width;
-    height = stage.canvas.height;
+    stage.canvas.style.cursor = "none";
+    set_size_from_canvas();
 
     //initialize player
     let circle1 = new createjs.Shape();
@@ -37,7 +52,7 @@ function init() {
 
     //REMOVE THESE IN FINAL
     let hit_circle = new createjs.Shape();
-    hit_circle.graphics.beginFill("White").drawCircle(0, 0, attack_radius);
+    hit_circle.graphics.beginFill("White").drawCircle(0, 0, hit_radius);
     hit_circle.alpha = 0.2;
 
     let hurt_circle = new createjs.Shape();
@@ -61,8 +76,10 @@ function init() {
     background.graphics.beginFill("#4e1e10").drawRect(0, 0, width, height);
 
     stage.addChild(background);
-
+    stage.addChild(trail_layer);
+    stage.addChild(attack_layer);
     stage.addChild(player);
+    stage.addChild(roach_layer);
 
     stage.addEventListener("stagemousemove", update_mouse, false); 
 
@@ -108,6 +125,27 @@ function update(event) {
             //kill player
             console.log("player hit!");
         }
+        if(within_circle(
+            roaches[i].display_object.x,
+            roaches[i].display_object.y,
+            player.x,
+            player.y,
+            hit_radius
+        )) {
+            //add new attack
+            let attack_exists = false;
+            for (i in attacks) {
+                if(attacks[i].roach === roaches[i]) {
+                    attack_exists = true;
+                    break;
+                }
+            }
+
+            if(!attack_exists) {
+                add_attack(roaches[i]);
+            }
+        }
+
     }
 
     //update roach trails
@@ -120,7 +158,38 @@ function update(event) {
     }
 
     //update attacks
+    for (i = attacks.length - 1; i >= 0; i--) {
+        attacks[i].line_display_object.x = attacks[i].roach.display_object.x;
+        attacks[i].line_display_object.y = attacks[i].roach.display_object.y;
+        if(within_circle(
+            attacks[i].roach.display_object.x,
+            attacks[i].roach.display_object.y,
+            player.x,
+            player.y,
+            hit_radius
+        )) {
+            attacks[i].lifetime += delta_time;
+            if(attacks[i].lifetime >= attack_duration) {
+                attack_layer.removeChild(attacks[i].line_display_object);
+                attacks.splice(i,1);
+                console.log("killed roach!");
+                continue;
+            }
+            attacks[i].line_display_object.alpha = attacks[i].lifetime / attack_duration * 0.8 + 0.2;
+            aim_attack(attacks[i]);
+        }
+        else {
+            attacks[i].lifetime -= delta_time / 4;
+            if(attacks[i].lifetime < 0) {
+                attack_layer.removeChild(attacks[i].line_display_object);
+                attacks.splice(i,1);
+                continue;
+            }
+            attacks[i].line_display_object.alpha = 0;
+        }
+        
 
+    }
 
 
     stage.update();
@@ -133,7 +202,7 @@ function spawn_roach(roach) {
     roach.display_object.x = 20;
     roach.display_object.y = 45;
 
-    stage.addChild(roach.display_object);
+    roach_layer.addChild(roach.display_object);
 
     roaches.push(roach);
 }
@@ -193,6 +262,39 @@ function update_green_roach(roach, delta_time) {
 
 }
 
+function aim_attack(attack) {
+    var dx = player.x - attack.line_display_object.x;
+    var dy = player.y - attack.line_display_object.y;
+    var length = Math.sqrt(dx * dx + dy * dy);
+    var angle = Math.atan(dy / dx) * 180 / Math.PI;
+    if(dx < 0) {
+        angle = 180 + angle;
+    }
+
+    attack.line_display_object.scaleX = length;
+    attack.line_display_object.rotation = angle;
+
+}
+
+function add_attack(roach) {
+    var line = new createjs.Shape();
+    line.graphics.beginFill("White").drawRect(0, -4, 1, 8);
+    var dx = player.x - roach.display_object.x;
+    var dy = player.y - roach.display_object.y;
+    var angle = Math.atan(dy / dx) * 180 / Math.PI;
+    if(dx < 0) {
+        angle = 180 + angle;
+    }
+
+    attack_layer.addChild(line);
+
+    attacks.push({
+        line_display_object: line,
+        roach: roach,
+        lifetime: 0
+    });
+}
+
 function add_trail(x, y, dx, dy, color_str) {
     let shape = new createjs.Shape();
     let color = "#ffffff";
@@ -210,7 +312,7 @@ function add_trail(x, y, dx, dy, color_str) {
     shape.x = x;
     shape.y = y;
 
-    stage.addChild(shape);
+    trail_layer.addChild(shape);
     roach_trails.push({
         display_object: shape,
         lifetime: 0
@@ -221,7 +323,7 @@ function within_circle(x, y, circle_x, circle_y, radius) {
     let dx = circle_x - x;
     let dx2 = dx * dx;
     let dy = circle_y - y;
-    let dy2 = dy - dy;
+    let dy2 = dy * dy;
 
     return radius * radius >= dx2 + dy2;
 }
